c9b142da3f245d497ea00169e4180c5f988e8658
nguyendangy <dangy1008@gmail.com>
2022-04-01 14:03:17 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:your_name_price/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +19,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Name Your Price',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ProducInfo {
+class ProductInfo {
   final String name;
   final double price;
-  ProducInfo(this.name, this.price);
+  ProductInfo(this.name, this.price);
 }
 
 final products = [
-  ProducInfo("Wireless mouse", 3),
-  ProducInfo("Keyboard", 5),
-  ProducInfo("Camera", 8),
-  ProducInfo("Speaker", 4),
-  ProducInfo("iPad", 1200),
+  ProductInfo("Wireless mouse", 3),
+  ProductInfo("Keyboard", 5),
+  ProductInfo("Camera", 8),
+  ProductInfo("Speaker", 4),
+  ProductInfo("iPad", 1200),
 ];
 
 class HomePage extends StatefulWidget {
@@ -24,59 +24,63 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentProductIndex = 0;
-  int? _inputtedPrice;
+  int? _intputtedPrice;
   String _result = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(children: [
-        const Padding(padding: EdgeInsets.only(top: 40)),
-        Text(products[_currentProductIndex].name),
-        const Padding(padding: EdgeInsets.only(top: 40)),
-        SizedBox(
-          width: 200,
-          child: TextField(
-            key: const Key('priceInput'),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            onChanged: (value) {
-              _inputtedPrice = int.tryParse(value);
-            },
-          ),
+      body: Center(
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.only(top: 40)),
+            Text(products[_currentProductIndex].name),
+            const Padding(padding: EdgeInsets.only(top: 40)),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                key: const Key('priceInput'),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                onChanged: (value) {
+                  _intputtedPrice = int.tryParse(value);
+                },
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 40)),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _result =
+                        _intputtedPrice == products[_currentProductIndex].price
+                            ? 'pass'
+                            : 'fail';
+                  });
+                },
+                child: const Text("Check")),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: Text(
+                _result,
+                key: const Key('result'),
+              ),
+            ),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _result = '';
+                    if (_currentProductIndex < 4) {
+                      _currentProductIndex++;
+                    }
+                  });
+                },
+                child: const Text("Next"),
+              ),
+            ),
+          ],
         ),
-        const Padding(padding: EdgeInsets.only(top: 40)),
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _result = _inputtedPrice == products[_currentProductIndex].price
-                    ? 'pass'
-                    : 'fail';
-              });
-            },
-            child: const Text("Check")),
-        Visibility(
-          visible: _result.isNotEmpty,
-          child: Text(
-            _result,
-            key: const Key('result'),
-          ),
-        ),
-        Visibility(
-          visible: _result.isNotEmpty,
-          child: ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _result = '';
-                if (_currentProductIndex < 4) {
-                  _currentProductIndex++;
-                }
-              });
-            },
-            child: const Text('Next'),
-          ),
-        ),
-      ]),
-    ));
+      ),
+    );
   }
 }
